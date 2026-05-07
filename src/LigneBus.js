d2877281b94f4ed946e0a1771ff5dfdc1502f1f9
Cheikh Mbacké Diene <cheikhmbackediene@esp.sn>
2026-05-07 22:22:33 +0000

    Lab 3 : etat, recherche et selection de lignes

--- src/LigneBus.js
+++ src/LigneBus.js
@@ -1,14 +1,24 @@
-import './LigneBus.css';
+import "./LigneBus.css";
 
-function LigneBus({ numero, depart, arrivee, arrets }) {
+function LigneBus({
+  numero,
+  depart,
+  arrivee,
+  arrets,
+  estSelectionnee,
+  onClick,
+}) {
   return (
-    <div className="ligne-bus">
+    <div
+      className={`ligne-bus ${estSelectionnee ? "ligne-bus-active" : ""}`}
+      onClick={onClick}
+    >
       <div className="ligne-numero">{numero}</div>
       <div className="ligne-info">
         <span className="ligne-trajet">
-          {depart} &rarr; {arrivee}
+          {depart} → {arrivee}
         </span>
-        <span className="ligne-arrets">{arrets} arrets</span>
+        <span className="ligne-arrets">{arrets} arrêts</span>
       </div>
     </div>
   );
